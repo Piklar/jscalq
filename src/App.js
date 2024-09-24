c0e49f5019ebaa84393698baefa05fa8b40f7e72
Piklar <ernzmanalo2003@gmail.com>
2024-09-24 12:08:32 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
+function Key({label, clickHandler}) {
+  return (
+    <button onClick={clickHandler}>
+      {label}
+    </button>
+  );
+}
+
+function Display() {
+  return (
+    <>0</>
+  );
+}
+
 function App() {
+
+  const clickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    alert(value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Calc">
+        <div className="Dsply">
+          <Display />
+        </div>
+
+        <div className="Bttns">
+        <Key label={7} clickHandler={clickHandler} />
+          <Key label={8} clickHandler={clickHandler} />
+          <Key label={9} clickHandler={clickHandler} />
+          <Key label={"/"} clickHandler={clickHandler} />
+          <Key label={4} clickHandler={clickHandler} />
+          <Key label={5} clickHandler={clickHandler} />
+          <Key label={6} clickHandler={clickHandler} />
+          <Key label={"*"} clickHandler={clickHandler} />
+          <Key label={1} clickHandler={clickHandler} />
+          <Key label={2} clickHandler={clickHandler} />
+          <Key label={3} clickHandler={clickHandler} />
+          <Key label={"-"} clickHandler={clickHandler} />
+          <Key label={"CLR"} clickHandler={clickHandler} />
+          <Key label={0} clickHandler={clickHandler} />
+          <Key label={"="} clickHandler={clickHandler} />
+          <Key label={"+"} clickHandler={clickHandler} />
+
+        </div>
+      </div>
     </div>
   );
 }
